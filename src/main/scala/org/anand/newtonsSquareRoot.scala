69c050dbba09c516d79724a798388b5863551160
sahil anand <sahilanand2025@gmail.com>
2016-08-18 01:32:27 +0200

    blocking and lexical scoping root function

--- src/main/scala/org/anand/newtonsSquareRoot.scala
+++ src/main/scala/org/anand/newtonsSquareRoot.scala
@@ -4,21 +4,23 @@ package org.anand
   * Sqare root algorithm with newtons method.
   */
 object newtonsSquareRoot {
+  def sqrt(x: Double) = {
+    def abs(x: Double) = if(x < 0) -x else x
 
-  def abs(x: Double) = if(x < 0) -x else x;
+    def squareItr(guess: Double) : Double =
+      if (isGoodEnough(guess)) {
+        guess
+      } else {
+        squareItr(improve(guess))
+      }
 
-  def squareItr(guess: Double, x: Double) : Double =
-    if (isGoodEnough(guess, x)) {
-      guess
-    } else {
-      squareItr(improve(guess, x), x)
-    }
+    def isGoodEnough(guess: Double) =
+      abs(guess * guess - x) / x < 0.001
 
-  def isGoodEnough(guess: Double, x: Double) =
-    abs(guess * guess - x) / x < 0.001
+    def improve(guess: Double) =
+      (guess + (x / guess)) / 2
 
-  def improve(guess: Double, x: Double) =
-    (guess + x / guess) / 2
+    squareItr(1.0);
+  }
 
-  def sqrt(x: Double) = squareItr(1.0, x);
 }
